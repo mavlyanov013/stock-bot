0bbca9bca7a2afe4e78e6ed1c75ffc4d6dcb7eb3
mavlyanov013 <mavlyanovrahmatillo2@gmail.com>
2026-06-10 02:56:51 +0500

    add --loop flag for continuous monitoring

--- app/Console/Commands/CheckStocksCommand.php
+++ app/Console/Commands/CheckStocksCommand.php
@@ -7,18 +7,32 @@ use Illuminate\Console\Command;
 
 class CheckStocksCommand extends Command
 {
-    protected $signature = 'stocks:check';
+    protected $signature = 'stocks:check {--loop : Run continuously with 30 second intervals}';
 
     protected $description = 'Check UZSE stock prices and send Telegram alerts';
 
     public function handle(StockMonitorService $monitor): int
+    {
+        if ($this->option('loop')) {
+            $this->info('Starting continuous monitoring (30s interval). Press Ctrl+C to stop.');
+
+            while (true) {
+                $this->runCheck($monitor);
+                sleep(30);
+            }
+        }
+
+        $this->runCheck($monitor);
+
+        return self::SUCCESS;
+    }
+
+    private function runCheck(StockMonitorService $monitor): void
     {
         $this->info('Checking UZSE stock prices...');
 
         $monitor->check();
 
         $this->info('Done.');
-
-        return self::SUCCESS;
     }
 }
